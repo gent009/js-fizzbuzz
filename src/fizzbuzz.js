e4e4b0227e313eea9186520558d132afb1b5eb0e
gent009 <167322651+gent009@users.noreply.github.com>
2024-04-26 15:58:10 +0100

    fizzbuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,15 @@
 // TODO: Write your function in this file
+function fizzBuzz (lower, upper){
+const result = [];
+for (let i = lower; i<= upper; i++){
 
+    let output ='';
+    if (i % 3 === 0) output += 'Fizz';
+    if (i % 5 === 0) output += 'Buzz';
+    result.push(output || i);
+}
+return result;
+}
+console.log(fizzBuzz(1, 30));
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzz
